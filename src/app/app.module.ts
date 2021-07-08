e731425401dabf96619ea5eec4f58fcefcf18268
Hov26 <hov.ghevondyan1994@gmail.com>
2021-07-09 00:49:49 +0400

    change html to angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SwitcherComponent } from './switcher/switcher.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { TimeComponent } from './time/time.component';
+import { NotificationComponent } from './notification/notification.component';
+import { InputComponent } from './input/input.component';
+import { TextareaComponent } from './textarea/textarea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SwitcherComponent
+    SwitcherComponent,
+    CalendarComponent,
+    TimeComponent,
+    NotificationComponent,
+    InputComponent,
+    TextareaComponent
   ],
   imports: [
     BrowserModule
