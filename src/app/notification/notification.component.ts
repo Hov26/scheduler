492977fabe7b7c84a8c3a1afe85fe275a8e55b42
Hov26 <hov.ghevondyan1994@gmail.com>
2021-07-09 10:08:49 +0400

    add new component, fix mixins,

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notification.component.scss']
 })
 export class NotificationComponent implements OnInit {
+  @Input() className:string = "";
 
   constructor() { }
 
